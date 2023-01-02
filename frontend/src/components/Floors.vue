4381d973a776031185fce3f5df89f9491aca56f0
Alexmust2 <alex.must2003@mail.ru>
2023-01-02 17:07:32 +0300

    add new methods

--- frontend/src/components/Floors.vue
+++ frontend/src/components/Floors.vue
@@ -1,11 +1,11 @@
 <template>
-    <div class="container">
+    <div id="containter" class="container">
         <div class="first_floor-container">
             <div class="first_floor">
                 
             </div>
             <h2 style="margin-left: 15px;">1</h2>
-            <button  @click=" move(1)"><div class="circle"></div></button>
+            <button id="button_1" @click=" move(1), isActive(1)"><div class="circle"></div></button>
         </div>
 
         <div div class="second_floor-container">
@@ -13,7 +13,7 @@
                 
             </div>
             <h2 style="margin-left: 15px;">2</h2>
-            <button  @click=" move(2)"><div class="circle"></div></button>
+            <button  id="button_2" @click=" move(2), isActive(2)"><div class="circle"></div></button>
         </div>
 
         <div class="third_floor-container">
@@ -21,7 +21,7 @@
                 
             </div>
             <h2 style="margin-left: 15px;">3</h2>
-            <button  @click="move(3)" ><div class="circle"></div></button>
+            <button id="button_3"  @click="move(3), isActive(3)" ><div class="circle"></div></button>
         </div>
 
         <div class="fourth_floor-container">
@@ -29,7 +29,7 @@
                 
             </div>
             <h2 style="margin-left: 15px;">4</h2>
-            <button   @click="move(4)"><div class="circle"></div></button>
+            <button id="button_4"   @click="move(4),isActive(4)"><div class="circle"></div></button>
         </div>
 
         <div class="fifth_floor-container">
@@ -37,17 +37,18 @@
                 
             </div>
             <h2 style="margin-left: 15px;">5</h2>
-            <button id="btn" v-bind="queue[5]" @click="move(5)"><div  class="circle"></div></button>
+            <button id="button_5"  @click="move(5), isActive(5)"><div  class="circle"></div></button>
         </div>
 
 
     </div>
     <div style="display:flex; justify-content:center" class="elevator_container" id="lift">
-        <h2>{{ queue[queue.length - 1] }}</h2>
+        <h1>{{ numberqueue[0] }}</h1>
     </div>
 </template>
 
 <script>
+
 
     export default {
         data() {
@@ -60,7 +61,9 @@
                     "-720px": 5,
                 },
                 queue: [],
-                isActive: false,
+                buttonqueue: [],
+                numberqueue: [],
+                disabled: false,
 
             }
         },  
@@ -68,14 +71,88 @@
             move(value) {
                 const direction = Object.keys(this.stack).find(key => this.stack[key] === value)
                 const lift = document.getElementById("lift")
-                lift.style.transform = `translateY(${direction})`
-                this.queue.push(value)
                 const queue = this.queue
-                setTimeout(function(){
-                    queue.shift()
-                },3000)
-            }   
-    }
+                const numberqueue = this.numberqueue
+                console.log()
+                numberqueue.push(value)
+                queue.push(direction)
+                lift.style.transform = `translateY(${queue[0]})`
+                localStorage.setItem("resultat", JSON.stringify(this.queue));
+                },
+
+                isActive(value) {
+                    const button = document.getElementById(`button_${value}`)
+                    const buttonqueue = this.buttonqueue
+                    buttonqueue.push(value)
+                    console.log(buttonqueue)
+                    if (buttonqueue.includes(value) == true) {
+                        button.style.background = 'blue'
+                    }   else {
+                        button.style.background = 'white'
+                    }
+                },
+
+                removefromqueue(){
+                    const queue = this.queue
+                    const timer = window.setInterval(function(){
+                    if(queue.length > 0){
+                       queue.shift();
+                       document.getElementById("lift").style.transform = `translateY(${queue[0]})`
+                    }
+                    else
+                        window.clearInterval(timer);
+                    }, 6000)
+                },
+
+                disableActive() {
+                    const buttonqueue = this.buttonqueue
+                    const Activetimer = window.setInterval(function(){
+                    if(buttonqueue.length > 0){
+                        document.getElementById(`button_${buttonqueue[0]}`).style.background = 'white'
+                        buttonqueue.shift()
+                    }
+                    else
+                        window.clearInterval(Activetimer);
+                    },4500)
+                },
+                RemoveNumberFromQueue(){
+                    const numberqueue = this.numberqueue
+                    const numberqueuetimer = window.setInterval(function(){
+                    if(numberqueue.length > 0){
+                        numberqueue.shift()
+                    }
+                    else
+                        window.clearInterval(numberqueuetimer);
+                    }, 6000)
+                },
+
+                shake() {
+                    const queue = this.queue
+                    const shaketimer = window.setInterval(function(){
+                    if(queue.length > 0){
+                        setTimeout(function(){
+                            document.getElementById('lift').className = 'elevator_container'
+                        }, 2000)
+                        document.getElementById('lift').className = 'shake'
+
+                    }
+                    else
+                        window.clearInterval(shaketimer);
+                    }, 4500)
+                },
+                
+        },
+        beforeMount(){
+            this.removefromqueue()
+            this.RemoveNumberFromQueue()
+            this.disableActive()
+            this.shake()
+        },
+        mounted() {
+        },
+        created() {
+            this.queue = JSON.parse(localStorage.getItem("queue") || '[]')
+        },
 }
 </script>
 
@@ -185,6 +262,29 @@ button:active {
     left: 18px;
     top: -184px;
     transition: all 3s ease-in-out;
+}
+
+.shake {
+    animation: shake 0.82s cubic-bezier(0.36, 0.07, 0.19, 0.97) both infinite;
+    transform: translate3d(0, 0, 0);
+    width: 133px;
+    height: 180px;
+    position: relative;
+    top: -184px;
+    left: 18px;
+    background-color: aqua;
+    transition: all 3s ease-in-out;
+}
+
+@keyframes shake {
+  10%{background: rgb(25, 26, 26);}
+
+  50%{background: aqua;}
+
+  90%{background: rgb(25, 26, 26);}
+  
+  100%{background: aqua;}
+
 }
 
 </style>
--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,22 +1,4 @@
-const queue = {
-    1: 1,
-    2: 4,
-    "40px": 7,
-    4: 10,
-    5: 13,
-    "str": "string"
-}
+arr = [6, 1, 2, 3]
 
-// const keylist = Object.keys(queue)
+console.log(arr[0])
 
-// keylist.forEach(function(element, index){
-//     setTimeout(function(){
-//         console.log(queue[element])
-//     },index * 2000)
-// })
-
-function getkeybyvalue(object, value) {
-    return Object.keys(object).find(key => object[key] === value)
-}
-
-console.log(getkeybyvalue(queue, 7))
